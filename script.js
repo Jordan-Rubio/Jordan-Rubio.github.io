3f549850c05f96c25ce76872981bf64bdcee33a3
Jordan Rubio <jordan.j.rubio@gmail.com>
2026-02-05 23:25:52 -0800

    Site refresh for the first time in a while

--- script.js
+++
@@ -1,9 +0,0 @@
-$('button').click(function() {
-	var target = "#" + $(this).data("target");//defines target based on data-target
-	//handles displaying sections based on button clicks
-    $(".section").not(target).hide();
-    $(target).show();
-    //handles active class to button
-    $(this).addClass('active');
-    $('button').not(this).removeClass('active');//removes active class from button that already has it
-});
